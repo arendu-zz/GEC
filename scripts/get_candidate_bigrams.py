4534af647b55f2b4bd170d59372aa1aad6e5c5da
arendu <adithya.r@gmail.com>
2016-04-20 13:29:11 -0400

    prints all candidate bigrams for scoring

--- scripts/get_candidate_bigrams.py
+++ scripts/get_candidate_bigrams.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 __author__ = 'arenduchintala'
 import sys
 import codecs
@@ -5,10 +6,12 @@ from optparse import OptionParser
 import itertools
 
 reload(sys)
-sys.setdefaultencoding('utf-8')
+'''sys.setdefaultencoding('utf-8')
 sys.stdin = codecs.getreader('utf-8')(sys.stdin)
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 sys.stdout.encoding = 'utf-8'
+'''
+import pdb
 BOS = '<s>'
 EOS = '</s>'
 
@@ -23,7 +26,7 @@ if __name__ == '__main__':
     opt.add_option('--pf', dest='pf_file', default='')
 
     (options, _) = opt.parse_args()
-    if options.gec_raw_file == '' or options.gec_pos == '' or options.nf_file == '' or options.vf_file == '':
+    if options.gec_raw_file == '' or options.gec_pos_file == '' or options.nf_file == '' or options.vf_file == '' or options.df_file == '' or options.pf_file == '':
         sys.stderr.write('Usage: '
                          'python get_candidate_bigrams.py '
                          '-f [gec raw file] '
@@ -37,13 +40,15 @@ if __name__ == '__main__':
         pass
 
     sent_list = codecs.open(options.gec_raw_file, 'r', 'utf8').readlines()
-    pos_list = codecs.open(options.gec_pos, 'r', 'utf8').readlines()
+    pos_list = codecs.open(options.gec_pos_file, 'r', 'utf8').readlines()
     nf = dict((items.split()[0], items.split()[1:]) for items in codecs.open(options.nf_file, 'r', 'utf8').readlines())
     vf = dict((items.split()[0], items.split()[1:]) for items in codecs.open(options.vf_file, 'r', 'utf8').readlines())
-    df = [item.strip() for item in codecs.open(options.df_file, 'r', 'utf8').readlines()]
-    pf = [item.strip() for item in codecs.open(options.pf_file, 'r', 'utf8').readlines()]
-
+    df = [item.strip() for item in codecs.open(options.df_file, 'r', 'utf8').readlines()  if item.strip() != '']
+    pf = [item.strip() for item in codecs.open(options.pf_file, 'r', 'utf8').readlines() if item.strip() != '']
+    pdb.set_trace()
     for sent, pos_sent in zip(sent_list, pos_list):
+        sys.stderr.write('sent:' + sent)
+        sys.stderr.write('pos:' + pos_sent)
         trellis = []
         trellis.append([BOS])
         words = sent.strip().split()
@@ -54,10 +59,10 @@ if __name__ == '__main__':
             elif p.startswith('DT') or p.startswith('RB'):
                 trellis.append(df)
             elif p.startswith('NN'):
-                detxnf = list(itertools.product(df + [' '], nf[w]))
+                detxnf = [' '.join(item) for item in itertools.product(df + [' '], nf[w])]
                 trellis.append(detxnf)
             elif p.startswith('JJ'):
-                detxjj = list(itertools.product(df + [' '], [w]))
+                detxjj = [' '.join(item) for item in itertools.product(df + [' '], [w])]
                 trellis.append(detxjj)
             else:
                 trellis.append([w])
