608927d109ca5712c7de53da40d9e23b2c1fbab1
arendu <adithya.r@gmail.com>
2016-04-21 18:35:59 -0400

    renamed pronoun file, removed phrasal artordet candidates

--- scripts/get_candidate_bigrams.py
+++ scripts/get_candidate_bigrams.py
@@ -23,27 +23,31 @@ if __name__ == '__main__':
     opt.add_option('--vf', dest='vf_file', default='')
     opt.add_option('--df', dest='df_file', default='')
     opt.add_option('--pf', dest='pf_file', default='')
+    opt.add_option('--prof', dest='prof_file', default='')
 
     (options, _) = opt.parse_args()
-    if options.gec_raw_file == '' or options.gec_pos_file == '' or options.nf_file == '' or options.vf_file == '' or options.df_file == '' or options.pf_file == '':
+    if options.gec_raw_file == '' or options.gec_pos_file == '' or options.nf_file == '' or options.vf_file == '' or options.df_file == '' or options.pf_file == '' or options.prof_file == '':
         sys.stderr.write('Usage: '
-                         'python get_candidate_bigrams.py '
-                         '-f [gec raw file] '
-                         '-p [pos file] '
-                         '--nf [nf file] '
-                         '--df [df file] '
-                         '--pf [pf file] '
-                         '--vf [vf file]\n')
+                'python get_candidate_bigrams.py '
+                '-f [gec raw file] '
+                '-p [pos file] '
+                '--nf [nf file] '
+                '--df [df file] '
+                '--pf [preposition file] '
+                '--prof [pronuon file] '
+                '--vf [vf file]\n')
         exit(1)
     else:
         pass
     d = enchant.Dict('en_US')
+    seen = {}
     sent_list = codecs.open(options.gec_raw_file, 'r', 'utf8').readlines()
     pos_list = codecs.open(options.gec_pos_file, 'r', 'utf8').readlines()
     nf = dict((items.split()[0], items.split()[1:]) for items in codecs.open(options.nf_file, 'r', 'utf8').readlines())
     vf = dict((items.split()[0], items.split()[1:]) for items in codecs.open(options.vf_file, 'r', 'utf8').readlines())
     df = [item.strip() for item in codecs.open(options.df_file, 'r', 'utf8').readlines()  if item.strip() != '']
     pf = [item.strip() for item in codecs.open(options.pf_file, 'r', 'utf8').readlines() if item.strip() != '']
+    prof = [item.strip() for item in codecs.open(options.prof_file, 'r', 'utf8').readlines() if item.strip() != '']
     for sent, pos_sent in zip(sent_list, pos_list):
         sys.stderr.write('sent:' + sent)
         sys.stderr.write('pos:' + pos_sent)
@@ -52,16 +56,18 @@ if __name__ == '__main__':
         words = sent.strip().split()
         pos = pos_sent.strip().split()
         for i, (w, p) in enumerate(zip(words, pos)):
-            if p.startswith('VB') and d.check(w):
+            if p.startswith('VB') and d.check(w) and w in vf:
                 trellis.append(vf[w])
             elif p.startswith('DT') or p.startswith('RB'):
                 trellis.append(df)
-            elif p.startswith('NN') and d.check(w):
+            elif p.startswith('NN') and d.check(w) and w in nf:
                 detxnf = [' '.join(item) for item in itertools.product(df + [' '], nf[w])]
                 trellis.append(detxnf)
             elif p.startswith('JJ'):
                 detxjj = [' '.join(item) for item in itertools.product(df + [' '], [w])]
                 trellis.append(detxjj)
+            elif p.startswith('PRP'):
+                trellis.append(prof)
             else:
                 trellis.append([w])
         trellis.append([EOS])
@@ -70,4 +76,10 @@ if __name__ == '__main__':
             current_candidates = trellis[i]
             for pc in prev_candidates:
                 for cc in current_candidates:
-                    print pc, '|||', cc
+                    print_line = pc + ' ||| '+ cc
+                    if print_line in seen:
+                        pass
+                    else:
+                        seen[print_line] = 1
+                        print print_line
+    sys.stderr.write('done\n')
--- scripts/qsub-get-cb.sh
+++ scripts/qsub-get-cb.sh
@@ -3,21 +3,13 @@
 #$ -S /bin/bash
 #$ -M adithya.renduchintala@jhu.edu
 #$ -m eas
-#$ -l mem_free=10G,ram_free=10G
+#$ -l mem_free=50G,ram_free=50G
 #$ -V
-#$ -j y -o /home/arenduc1/Projects/GEC/scripts/qsub.cb.log
 set -e
 . ~/.bashrc
 source ~/.profile
 GEC_HOME="/home/arenduc1/Projects/GEC"
-$GEC_HOME/scripts/get_candidate_bigrams.py -f $GEC_HOME/data/gec/train/conll14st-preprocessed.m2.mod.raw -p $GEC_HOME/data/gec/train/conll14st-preprocessed.m2.mod.pos --nf $GEC_HOME/data/candidates/nform_candidates --vf $GEC_HOME/data/candidates/vform_candidates --df $GEC_HOME/data/candidates/artordet_candidates --pf $GEC_HOME/data/candidates/prep_candidates > train.cb
-$GEC_HOME/scripts/get_candidate_bigrams.py -f $GEC_HOME/data/gec/dev/official-preprocessed.m2.mod.raw -p $GEC_HOME/data/gec/dev/official-preprocessed.m2.mod.pos --nf $GEC_HOME/data/candidates/nform_candidates --vf $GEC_HOME/data/candidates/vform_candidates --df $GEC_HOME/data/candidates/artordet_candidates --pf $GEC_HOME/data/candidates/prep_candidates > dev.cb
-$GEC_HOME/scripts/get_candidate_bigrams.py -f $GEC_HOME/data/gec/dev/official-preprocessed.5types.m2.mod.raw -p $GEC_HOME/data/gec/dev/official-preprocessed.5types.m2.mod.pos --nf $GEC_HOME/data/candidates/nform_candidates --vf $GEC_HOME/data/candidates/vform_candidates --df $GEC_HOME/data/candidates/artordet_candidates --pf $GEC_HOME/data/candidates/prep_candidates > dev.5types..cb
-$GEC_HOME/scripts/get_candidate_bigrams.py -f $GEC_HOME/data/gec/test/official-2014.combined.m2.mod.raw -p $GEC_HOME/data/gec/test/official-2014.combined.m2.mod.pos --nf $GEC_HOME/data/candidates/nform_candidates --vf $GEC_HOME/data/candidates/vform_candidates --df $GEC_HOME/data/candidates/artordet_candidates --pf $GEC_HOME/data/candidates/prep_candidates > test.cb
-touch $GEC_HOME/scripts/all.cb
-cat $GEC_HOME/scripts/train.cb >> $GEC_HOME/scripts/all.cb
-cat $GEC_HOME/scripts/dev.5types.cb >> $GEC_HOME/scripts/all.cb
-cat $GEC_HOME/scripts/dev.cb >> $GEC_HOME/scripts/all.cb
-cat $GEC_HOME/scripts/test.cb >> $GEC_HOME/scripts/all.cb
-cat $GEC_HOME/scripts/all.cb | sort | uniq > $GEC_HOME/data/candidates/candidate_bigrams
+INP_FILE=$1
+OUT_FILE=$2
+$GEC_HOME/scripts/get_candidate_bigrams.py -f $INP_FILE.raw -p $INP_FILE.pos --nf $GEC_HOME/data/candidates/nform_candidates --vf $GEC_HOME/data/candidates/vform_candidates --df $GEC_HOME/data/candidates/artordet_candidates --pf $GEC_HOME/data/candidates/prep_candidates --prof $GEC_HOME/data/candidates/pronoun_candidates 2> $OUT_FILE.log > $OUT_FILE.cb
 
