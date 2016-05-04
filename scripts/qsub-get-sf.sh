b18206075fdfc551352b5d675f89eb5c2d926358
arendu <adithya.r@gmail.com>
2016-05-04 16:45:47 -0400

    updated links

--- scripts/qsub-get-sf.sh
+++ scripts/qsub-get-sf.sh
@@ -12,4 +12,4 @@ source ~/.profile
 GEC_HOME="/home/arenduc1/Projects/GEC"
 INP_FILE=$GEC_HOME/data/gec/all.data
 OUT_FILE=$GEC_HOME/data/gec/all.data
-$GEC_HOME/scripts/get_sparse_features.py -f $INP_FILE.raw -p $INP_FILE.pos --nf $GEC_HOME/data/candidates/nform_candidates --vf $GEC_HOME/data/candidates/vform_candidates --df $GEC_HOME/data/candidates/artordet_candidates --pf $GEC_HOME/data/candidates/prep_candidates --prof $GEC_HOME/data/candidates/pronoun_candidates --bf $GEC_HOME/data/gec/giga.lm.1.bigram.only  --uf $GEC_HOME/data/gec/giga.lm.1.unigram.only 2> $OUT_FILE.log > $OUT_FILE.sf
+$GEC_HOME/scripts/get_sparse_features.py -f $INP_FILE.raw -p $INP_FILE.pos --nf $GEC_HOME/data/candidates/nform_candidates --vf $GEC_HOME/data/candidates/vform_candidates --df $GEC_HOME/data/candidates/artordet_candidates --pf $GEC_HOME/data/candidates/prep_candidates --prof $GEC_HOME/data/candidates/pronoun_candidates --bf $GEC_HOME/data/gec/giga.train.interpolated.lm.limited.vocab.lower.bigrams  --uf $GEC_HOME/data/gec/giga.train.interpolated.lm.limited.vocab.lower.unigrams 2> $OUT_FILE.log > $OUT_FILE.sf
